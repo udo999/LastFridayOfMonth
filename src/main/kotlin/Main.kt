f0998a46916fc02a9f226ca284f69bc7f9d3d2fd
Udi <zeljkoudovicic@gmail.com>
2023-05-20 23:50:52 +0200

    Final project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun getLastFridayOfMonth(year: Int) {
+    for (month in Month.values()) {
+        val firstOfMonth = LocalDate.of(year, month, 1)
+        val firstWeekday = firstOfMonth.dayOfWeek
+        val daysInMonth = month.length(firstOfMonth.isLeapYear)
+
+        val lastFridayDay: Int = when (firstWeekday) {
+            DayOfWeek.SUNDAY -> 27
+            DayOfWeek.MONDAY -> 26
+            DayOfWeek.TUESDAY -> 25
+            DayOfWeek.WEDNESDAY -> if (daysInMonth == 31) 31 else 24
+            DayOfWeek.THURSDAY -> if (daysInMonth >= 30) 30 else 23
+            DayOfWeek.FRIDAY -> if (daysInMonth >= 29) 29 else 22
+            DayOfWeek.SATURDAY -> 28
+        }
+
+        val lastFriday = LocalDate.of(year, month, lastFridayDay)
+        println("$lastFriday (${lastFriday.dayOfWeek}), ${lastFriday.year}")
+    }
+}
+
+fun main() {
+    val year = 2023
+    getLastFridayOfMonth(year)
 }
